423b132d034a3861569cd159c630d53e4ad53d0b
Rohit Kumar <rohitgemini1992@gmail.com>
2025-04-18 15:19:10 +0530

    updates

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get("/api/hello", function (req, res) {
 });
 
 //timestamp microservice endpoint
-app.get("/api/timestamp/:date?", function (req, res) {
+app.get("/api/:date?", function (req, res) {
   const dateParam = req.params.date;
   let date;
 });
